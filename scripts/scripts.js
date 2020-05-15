9cf1039d27a5b60678609fad13991c1d5891a5cb
Luke <lflint07@gmail.com>
2020-05-15 14:11:55 -0400

    added javascript to the modals in the About section. Currently the modals won't close

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,10 @@
-$(".modal-button").click(function(){
-    var target = $(this).data("target");
-    $("html").addClass("is-clipped");
-    $(target).addClass("is-active");
-});
+// $(".modal-button").click(function(){
+//     var target = $(this).data("target");
+//     $("html").addClass("is-clipped");
+//     $(target).addClass("is-active");
+// });
 
-$("modal-close").click(function() {
-    $("html").removeClass("is-clipped");
-    $(target).parent().removeClass("is-active");
-});
+// $("modal-close").click(function() {
+//     $("html").removeClass("is-clipped");
+//     $(target).parent().removeClass("is-active");
+// });
